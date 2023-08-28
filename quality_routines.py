27c3e51bef7e70b09ecf098f9385a36c7dd471a7
Antoine Déchappe <dechantoine@gmail.com>
2023-08-28 15:58:18 +0200

    feat(VOCABULARY): add tqdm

---
+++ quality_routines.py
@@ -0,0 +1,8 @@
+# TO DO:
+# detect + count + extract web links
+# detect + count + extract email addresses
+# detect + count + extract social media handles
+# detect + count + extract phone numbers
+# analyse the longest words
+# analyse the longest sentences
+# detect + count + characters repetition
