1573a452e78b9a963724ff2c188d96804dbf2897
Antoine Déchappe <dechantoine@gmail.com>
2023-09-01 14:24:45 +0200

    feat(VOCABULARY): add parallelization

--- vocabulary_analysis.py
+++ vocabulary_analysis.py
@@ -3,39 +3,47 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+from joblib import Parallel, delayed
+
+def parallel_distance(s1, list2):
+    distances = [distance(s1, s2, score_cutoff=distance_threshold) for s2 in list2]
+
+    return [np.argpartition(distances, 1)[1],
+            np.partition(distances, 1)[1]]
+
+
 def compute_closest_words(voc1, voc2=None, threshold=None):
     """Compute closest words.
 
         Args:
-            voc1(array-like): Array of words
-            voc2(array-like): Array of words (voc1 is compared to voc2 if provided)
-            threshold(float): Maximum distance between s1 and s2, that is considered as a result. If the distance is bigger than score_cutoff, score_cutoff + 1 is returned instead
+            voc1(List[str]): List of words
+            voc2(List[str]): List of words (voc1 is compared to voc2 if provided)
 
         Returns:
-            closest_words(pd.DataFrame): DataFrame of the closest word from voc1 in voc1 (voc2) with associated distance
+            closest_words(pd.DataFrame): DataFrame of closest word from voc1 in voc1 (voc2) with associated distance
     """
+
+    global distance_threshold
+    distance_threshold = threshold
 
     if not voc2:
         voc2 = voc1
 
-    distance_func = distance
+    distances = Parallel(n_jobs=20)(delayed(parallel_distance)(voc1[i], voc2) for i in tqdm(range(len(voc1))))
 
-    distances = np.array([[voc1[i],
-                           voc2[j],
-                           distance_func(voc1[i],
-                                         voc2[j],
-                                         score_cutoff=threshold)]
-                          for j in tqdm(range(len(voc1)))
-                          for i in range(j if np.array_equal(voc1, voc2)
-                                         else len(voc2))]
-                         )
-
-    closest_words = (pd.DataFrame(data=np.concatenate((distances, distances[:, [1, 0, 2]])),
-                                  columns=["word1", "word2", "distance"])
-                     .sort_values(by=["word1", "distance"],
-                                  ascending=True)
-                     .drop_duplicates(subset=["word1"])
-                     .reset_index(drop=True)
+    closest_words = (pd.DataFrame(distances)
+                     .merge(right=pd.Series(voc1,
+                                            name="word_voc1"),
+                            left_index=True,
+                            right_index=True,
+                            how="left")
+                     .merge(right=pd.Series(voc1,
+                                            name="closest_word_voc2"),
+                            left_on=0,
+                            right_index=True,
+                            how="left")
+                     .drop(columns=[0])
+                     .rename(columns={1: "distance"})
                      )
 
     return closest_words
